ffd7c8ac27573cf4ee9f50d8826c3af102ffcc5a
sufjanjoy-arch <sufjanjoy@gmail.com>
2026-07-16 10:47:47 +0530

    Update vite.config.ts
    edit on vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cn-resume/",
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [tailwindcss(), react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    tailwindcss(),
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
